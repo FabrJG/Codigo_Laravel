2302389f8722a2b7f8e0315b59e83637d4b3e5d8
Fabricio Julian <fabricio.jg2507@gmail.com>
2024-06-27 11:57:55 -0500

    Codigo Laravel Semana 08

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,7 @@ Route::post('servicios','App\Http\Controllers\ServiciosController@store')->name(
 Route::get('servicios/{id}','App\Http\Controllers\ServiciosController@show')->name('servicios.show');
 Route::delete('servicios/{servicio}','App\Http\Controllers\ServiciosController@destroy')->name('servicios.destroy');
 Route::view('contacto','contacto')->name('contacto');
+
+Route::post('contacto','App\Http\Controllers\ContactoController@store');
+
 //Route::resource('servicios','App\Http\Controllers\Servicios3Controller')/*->except('index','show')*/;
